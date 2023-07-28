a2808c89df2db284860041d5dbd289e9b00498fc
team manager <root@test.pri>
2023-07-28 16:46:36 +0900

    Mongodb와의 연결 - 1

--- routers/webs.py
+++ routers/webs.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
-from pymongo.collection import Collection
-from bson import ObjectId
+from beanie import PydanticObjectId 
+from database.connection import Database
+from models.otas import OTA
 
 router = APIRouter()
-ota_collection = Collection  # MongoDB collection 객체
+ota_database = Database(OTA)
 
 templates = Jinja2Templates(directory='templates/')
 
@@ -12,7 +13,7 @@ templates = Jinja2Templates(directory='templates/')
 @router.post('/')
 @router.delete('/')
 async def mainPage(request: Request):
-    otas = await ota_collection.find().to_list(length=None)
+    otas = await ota_database.get_all()
     return templates.TemplateResponse(
         "main.html",
         {
